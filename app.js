a29326b6c54346539a3933c0df46e858be8f4afd
Marta Kule <marta.kule@gmail.com>
2019-12-23 23:07:29 -0500

    Trying out a response status code

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world, this is Morta Della.");
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world, this is Morta Della.<h1>');
 });
 
 app.listen(port, () => console.log(`The app is listening on port ${port}.`));
+
+app.get('/statuscode', (req, res) => {
+  res.set('X-custom-header', 'looks like this');
+  res.status(418);
+  res.send('I have a server')
+});
